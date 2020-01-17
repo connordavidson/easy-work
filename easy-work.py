33137a4c9e4acc03deb9ba2920350335153a0fc8
Connor Davidson <connor.j.davidson@gmail.com>
2020-01-17 16:41:03 -0500

    read a .txt file line by line and ping the website on each line

--- easy-work.py
+++ easy-work.py
@@ -1,15 +1,9 @@
 import os
-# from pathlib import Path
-
+#opens the file (in read mode) and prepares the file to be read line by line
 f = open('websites.txt', mode='r')
-
 file = f.readlines()
-
+#loop through every line in the file and ping the website on that line. Has to use [ :-1] because the last character of every line is a newline character (\n). This messes up the command variable and puts it onto 2 lines
 for website in file:
-
-
-    print("PINGING: " + website)
-    os.system("ping " + website + " -c 1")
-
-
-# os.system("date")
+    command = "ping %s -c 1" % website[ : -1]
+    print("PINGING: " + website[ : -1] )
+    os.system(command)
