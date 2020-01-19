2f25325a42bf27b6c89f28823e5a673d22df4f63
Connor Davidson <connor.j.davidson@gmail.com>
2020-01-19 13:53:38 -0500

    increments by drive number

--- easy-work.py
+++ easy-work.py
@@ -3,6 +3,7 @@ import os, time
 f = open('websites.txt', mode='r')
 file = f.readlines()
 #loop through every line in the file and ping the website on that line. Has to use [ :-1] because the last character of every line is a newline character (\n). This messes up the command variable and puts it onto 2 lines
+drive_number = 1
 for website in file:
     command = "ping %s -c 1" % website[ : -1]
     print("PINGING: " + website[ : -1] )
@@ -10,3 +11,4 @@ for website in file:
     #sleep for 3 seconds before running the command again
     time.sleep(3)
     os.system(command)
+    drive_number += 1
